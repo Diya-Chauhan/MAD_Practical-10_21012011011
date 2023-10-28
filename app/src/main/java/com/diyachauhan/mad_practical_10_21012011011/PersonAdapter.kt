26c6bd41d772f5fe0fb3b31598d2dd1d74126ac7
Diya Chauhan <diyachauha1234@gmail.com>
2023-10-28 10:17:55 +0530

    practical-10

--- app/src/main/java/com/diyachauhan/mad_practical_10_21012011011/PersonAdapter.kt
+++ app/src/main/java/com/diyachauhan/mad_practical_10_21012011011/PersonAdapter.kt
@@ -6,11 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.TextView
-import com.diyachauhan.mad_practical_10_21012011011.MapsActivity
-import com.diyachauhan.mad_practical_10_21012011011.Person
-import com.diyachauhan.mad_practical_10_21012011011.R
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class PersonAdapter(context: Context,val personArray:ArrayList<Person>):ArrayAdapter<Person>(context,0,personArray)
@@ -23,7 +20,7 @@ class PersonAdapter(context: Context,val personArray:ArrayList<Person>):ArrayAda
         view.findViewById<TextView>(R.id.mobile).text = contact?.phoneNo
         view.findViewById<TextView>(R.id.emailid).text = contact?.emailId
         view.findViewById<TextView>(R.id.address).text = contact?.address
-        view.findViewById<FloatingActionButton>(R.id.btnLocation).setOnClickListener{
+        view.findViewById<MaterialButton>(R.id.btnLocation).setOnClickListener{
              //Start the MapsActivity when button1 is clicked
             val intent = Intent(context, MapsActivity::class.java)
             context.startActivity(intent)
